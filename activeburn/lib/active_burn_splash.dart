5de4cc2f0dcd7e0456d3ea09655414f28d45f81f
teebee11 <111260058+teebee11@users.noreply.github.com>
2022-08-20 19:22:22 -0700

    Update active_burn_splash.dart

--- activeburn/lib/active_burn_splash.dart
+++ activeburn/lib/active_burn_splash.dart
@@ -1,4 +1,4 @@
-import 'package:active_burn_app/authentication/sign_in.dart'
+import 'package:active_burn_app/authentication/sign_in.dart';
 import 'package:flutter/material.dart';
 
 class StartUp extends StatelessWidget {
